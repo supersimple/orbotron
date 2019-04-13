421b663951daeb40f00dcd2875d5e7fd90fe14ec
Todd Resudek <toddresudek@gmail.com>
2019-04-12 22:29:13 -0600

    Test formatter

--- lib/orbotron.ex
+++ lib/orbotron.ex
@@ -6,4 +6,7 @@ defmodule Orbotron do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+def info() do
+ IO.puts("INFO")
+  end
 end
